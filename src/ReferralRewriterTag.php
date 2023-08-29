cce1713305e52bc917c569b0b1c9eced7ad485f5
Stefano Novelli <murdercode@gmail.com>
2023-08-29 11:28:40 +0200

    wip

--- src/ReferralRewriterTag.php
+++ src/ReferralRewriterTag.php
@@ -4,4 +4,46 @@ namespace The3LabsTeam\ReferralRewriterTag;
 
 class ReferralRewriterTag
 {
+    /**
+     *  Start function
+     */
+    public function rewrite(string $link, ?string $tag, ?string $subtag) : string {
+        return match($this->retrieveTheDomainType($link)) {
+            'amazon' => $this->rewriteAmazonLink($link, $tag, $subtag),
+//            'instant-gaming' => $this->rewriteInstantGamingLink($link, $tag, $subtag),
+            default => $link,
+        };
+    }
+
+    /**
+     *  Rewrite amazon link with tag and subtag
+     */
+    public function rewriteAmazonLink(string $link, ?string $tag, ?string $subtag) : string
+    {
+        $link = preg_replace('/tag=[^&]*/', 'tag=' . $tag, $link);
+        $link = preg_replace('/ascsubtag=[^&]*/', 'ascsubtag=' . $subtag, $link);
+        return $link;
+    }
+
+    /**
+     *  Rewrite instant gaming link with tag and subtag
+     */
+//    public function rewriteInstantGamingLink(string $link, ?string $tag, ?string $subtag) : string
+//    {
+//        $link = preg_replace('/igr=[^&]*/', 'igr=' . $tag, $link);
+//        $link = preg_replace('/igr_extra=[^&]*/', 'igr_extra=' . $subtag, $link);
+//        return $link;
+//    }
+
+    /**
+     *  Retrieve the domain type
+     */
+    public function retrieveTheDomainType(string $link): string
+    {
+        $domain = parse_url($link, PHP_URL_HOST);
+        $domain = str_replace('www.', '', $domain);
+        return explode('.', $domain)[0];
+
+    }
+
 }
---
+++ tests/AmazonRewriterTest.php
@@ -0,0 +1,28 @@
+<?php
+
+use The3LabsTeam\ReferralRewriterTag\Facades\ReferralRewriterTag;
+
+it('can rewrite amazon link with tag', function () {
+    $link = 'https://www.amazon.it/dp/B08PC3PGZM?tag=tomshar-21&linkCode=ogi';
+    $tag = 'mytag-20';
+
+    $rewrittenLink = ReferralRewriterTag::rewrite($link, $tag, null);
+    expect($rewrittenLink)->toBe('https://www.amazon.it/dp/B08PC3PGZM?tag=mytag-20&linkCode=ogi');
+});
+
+it('can rewrite amazon link with subtag', function () {
+    $link = 'https://www.amazon.it/dp/B08PC3PGZM?tag=tomshar-21&linkCode=ogi';
+    $subtag = 'customsub';
+
+    $rewrittenLink = ReferralRewriterTag::rewrite($link, null, $subtag);
+    expect($rewrittenLink)->toBe('https://www.amazon.it/dp/B08PC3PGZM?tag=tomshar-21&ascsubtag=customsub&linkCode=ogi');
+});
+
+it('can rewrite amazon link with tag and subtag', function () {
+    $link = 'https://www.amazon.it/dp/B08PC3PGZM?tag=tomshar-21&linkCode=ogi';
+    $tag = 'mytag-20';
+    $subtag = 'customsub';
+
+    $rewrittenLink = ReferralRewriterTag::rewrite($link, $tag, $subtag);
+    expect($rewrittenLink)->toBe('https://www.amazon.it/dp/B08PC3PGZM?tag=mytag-20&ascsubtag=customsub&linkCode=ogi');
+});
