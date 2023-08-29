7d32f275517c613e624ef9ce0941561e89e8a837
murdercode <murdercode@users.noreply.github.com>
2023-08-29 09:29:13 +0000

    Fix styling

--- src/ReferralRewriterTag.php
+++ src/ReferralRewriterTag.php
@@ -7,10 +7,11 @@ class ReferralRewriterTag
     /**
      *  Start function
      */
-    public function rewrite(string $link, ?string $tag, ?string $subtag) : string {
-        return match($this->retrieveTheDomainType($link)) {
+    public function rewrite(string $link, ?string $tag, ?string $subtag): string
+    {
+        return match ($this->retrieveTheDomainType($link)) {
             'amazon' => $this->rewriteAmazonLink($link, $tag, $subtag),
-//            'instant-gaming' => $this->rewriteInstantGamingLink($link, $tag, $subtag),
+            //            'instant-gaming' => $this->rewriteInstantGamingLink($link, $tag, $subtag),
             default => $link,
         };
     }
@@ -18,22 +19,23 @@ class ReferralRewriterTag
     /**
      *  Rewrite amazon link with tag and subtag
      */
-    public function rewriteAmazonLink(string $link, ?string $tag, ?string $subtag) : string
+    public function rewriteAmazonLink(string $link, ?string $tag, ?string $subtag): string
     {
-        $link = preg_replace('/tag=[^&]*/', 'tag=' . $tag, $link);
-        $link = preg_replace('/ascsubtag=[^&]*/', 'ascsubtag=' . $subtag, $link);
+        $link = preg_replace('/tag=[^&]*/', 'tag='.$tag, $link);
+        $link = preg_replace('/ascsubtag=[^&]*/', 'ascsubtag='.$subtag, $link);
+
         return $link;
     }
 
     /**
      *  Rewrite instant gaming link with tag and subtag
      */
-//    public function rewriteInstantGamingLink(string $link, ?string $tag, ?string $subtag) : string
-//    {
-//        $link = preg_replace('/igr=[^&]*/', 'igr=' . $tag, $link);
-//        $link = preg_replace('/igr_extra=[^&]*/', 'igr_extra=' . $subtag, $link);
-//        return $link;
-//    }
+    //    public function rewriteInstantGamingLink(string $link, ?string $tag, ?string $subtag) : string
+    //    {
+    //        $link = preg_replace('/igr=[^&]*/', 'igr=' . $tag, $link);
+    //        $link = preg_replace('/igr_extra=[^&]*/', 'igr_extra=' . $subtag, $link);
+    //        return $link;
+    //    }
 
     /**
      *  Retrieve the domain type
@@ -42,8 +44,8 @@ class ReferralRewriterTag
     {
         $domain = parse_url($link, PHP_URL_HOST);
         $domain = str_replace('www.', '', $domain);
+
         return explode('.', $domain)[0];
 
     }
-
 }
